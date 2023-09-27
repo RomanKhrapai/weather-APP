197f50947aac8982d9bc8069dec31faab8df7e6e
RomanKhrapai <hrapayr@gmail.com>
2023-09-27 14:44:35 +0300

    fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import { APPID } from "./defaultValue.js";
 export function fetchSityInfo(name) {
     return fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${APPID}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${APPID}`
     ).then((response) => {
         if (!response.ok) {
             throw new Error(response.status);
